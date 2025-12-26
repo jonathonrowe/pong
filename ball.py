a6cf0d7122772a9794178af8f4db2fc10a01f4bd
jonathonrowe <jonathonlrowe@gmail.com>
2025-12-26 16:57:56 -0600

    updated ball draw method

--- ball.py
+++ ball.py
@@ -1,8 +1,12 @@
 import pygame
-from constants import BALL_LENGTH
+from constants import BALL_LENGTH, WHITE
 from rectangleshape import RectangleShape
 
 class Ball(RectangleShape):
     def __init__(self, x, y):
         super().__init__(x, y, BALL_LENGTH, BALL_LENGTH)
-        
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, WHITE, (self.x, self.y, self.width, self.height))
+
+    
