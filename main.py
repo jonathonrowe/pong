cec819ccacc40f9071862c530a7f07d5874fba4a
jonathonrowe <jonathonlrowe@gmail.com>
2025-12-27 15:37:12 -0600

    began start screen

--- main.py
+++ main.py
@@ -30,11 +30,13 @@ def main():
     game_state = "start"
     running = True
 
-    def draw_text(text, font, color, surface):
+    def draw_text(text, font, color, surface, x, y):
         textobj = font.render(text, True, color)
         textrect = textobj.get_rect()
-        textrect.center = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+        textrect.center = (x, y)
         surface.blit(textobj, textrect)
+
+    title = "PONG by Jon"
 
     # Main Game Loop
     while running:
@@ -42,12 +44,18 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    game_state = "paused"
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    paused = "paused"
+                    game_state = "playing"
 
         screen.fill("black")
 
-        if game_state == "playing":
+        if game_state == "start":
+            draw_text(title, font, WHITE, screen, (SCREEN_WIDTH / 2), (SCREEN_HEIGHT * .2))
+        
+        elif game_state == "playing":
 
             updatable.update(dt)
 
@@ -66,7 +74,7 @@ def main():
             for sprite in drawable:
                 sprite.draw(screen)
         
-        else:
+        elif game_state == "paused":
             draw_text("PAUSED", font, WHITE, screen, (SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
         player1_score_surface = score_font.render(str(player1.score), True, WHITE)
